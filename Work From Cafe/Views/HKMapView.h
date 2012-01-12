390c2ff90a419503e88e37e1a59d9776b79ad6fa
Błażej Biesiada <blazej@biesiada.net.pl>
2012-01-12 22:17:44 +0100

    Added empty map view.

---
+++ Work From Cafe/Views/HKMapView.h
@@ -0,0 +1,19 @@
+//
+//  HKMapView.h
+//  Work From Cafe
+//
+//  Created by Błażej Biesiada on 1/12/12.
+//  Copyright (c) 2012 HackKrk. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
+
+
+@interface HKMapView : UIView
+
+
+@property (nonatomic, readonly, strong) MKMapView *mapView;
+
+
+@end
---
+++ Work From Cafe/Views/HKMapView.m
@@ -0,0 +1,35 @@
+//
+//  HKMapView.m
+//  Work From Cafe
+//
+//  Created by Błażej Biesiada on 1/12/12.
+//  Copyright (c) 2012 HackKrk. All rights reserved.
+//
+
+#import "HKMapView.h"
+
+
+@interface HKMapView () // Private
+@property (nonatomic, readwrite, strong) MKMapView *mapView;
+@end
+
+
+@implementation HKMapView
+
+
+@synthesize mapView = _mapView;
+
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self)
+    {
+        self.mapView = [[MKMapView alloc] initWithFrame:self.bounds];
+        self.mapView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
+    }
+    return self;
+}
+
+
+@end
